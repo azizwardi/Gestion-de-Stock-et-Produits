e3e24752b736578c0f3fa8edd9801b12a2fb3f63
ghofrane <mail teek>
2025-04-02 14:43:25 +0100

    eureka

--- src/main/java/tn/esprit/gestion_de_stock_et_produits/GestionDeStockEtProduitsApplication.java
+++ src/main/java/tn/esprit/gestion_de_stock_et_produits/GestionDeStockEtProduitsApplication.java
@@ -2,12 +2,13 @@ package tn.esprit.gestion_de_stock_et_produits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GestionDeStockEtProduitsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GestionDeStockEtProduitsApplication.class, args);
     }
-
 }
