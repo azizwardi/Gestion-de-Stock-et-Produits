25269a557ddcf25f530e9159896745f37947919f
azizwardi <wardiaziz2507@gmail.com>
2025-04-05 00:27:23 +0100

    eureka

--- src/main/java/tn/esprit/gestion_de_stock_et_produits/GestionDeUser.java
+++ src/main/java/tn/esprit/gestion_de_stock_et_produits/GestionDeUser.java
@@ -2,12 +2,15 @@ package tn.esprit.gestion_de_stock_et_produits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
 
 @SpringBootApplication
-public class GestionDeStockEtProduitsApplication {
+public class GestionDeUser {
 
     public static void main(String[] args) {
-        SpringApplication.run(GestionDeStockEtProduitsApplication.class, args);
+        SpringApplication.run(GestionDeUser.class, args);
     }
 
 }
